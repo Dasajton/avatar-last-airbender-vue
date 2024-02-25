e853a7d818d50d8c4512a0099e15f9f242b0e817
Dasajton <david.sajitz.94@gmail.com>
2024-02-25 20:32:44 +0100

    navbar is now top fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,14 @@
 <template>
-  <nav class="flex h-12 w-full items-center justify-between bg-slate-950 px-4 text-slate-100">
+  <nav
+    class="fixed top-0 flex h-12 w-full items-center justify-between bg-slate-950 px-4 text-slate-100 shadow-md shadow-blue-500"
+  >
     <h1 class="text-center text-lg font-bold text-white">The Last Airbender</h1>
     <ul class="flex items-center gap-4">
       <router-link to="/">Home</router-link>
       <router-link to="/characters">Characters</router-link>
     </ul>
   </nav>
+  <div class="h-16 w-full"></div>
 </template>
 
 <script>
