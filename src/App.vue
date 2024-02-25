a4a28cbbf4f907af2a52bb5f5eb32f0daf88c7c4
Dasajton <david.sajitz.94@gmail.com>
2024-02-25 20:38:20 +0100

    script now lives in Home.vue

--- src/App.vue
+++ src/App.vue
@@ -10,42 +10,6 @@ import { RouterView } from "vue-router"
 export default {
   components: {
     NavBar
-  },
-  data: () => ({
-    characterList: [
-      {
-        name: "Aang",
-        element: ["Air", "Earth", "Water", "Fire"]
-      },
-      {
-        name: "Zuko",
-        element: ["Fire"]
-      },
-      {
-        name: "Toph",
-        element: ["Earth"]
-      },
-      {
-        name: "Katara",
-        element: ["Water"]
-      }
-    ],
-    favoriteList: []
-  }),
-  methods: {
-    removeCharacter(character) {
-      this.characterList = this.characterList.filter((c) => c !== character)
-    },
-    addFavoriteCharacter(character) {
-      if (!this.favoriteList.includes(character)) {
-        this.favoriteList.push(character)
-      } else {
-        alert("Character is already in Favorites")
-      }
-    },
-    removeFavoriteCharacter(character) {
-      this.favoriteList = this.favoriteList.filter((c) => c !== character)
-    }
   }
 }
 </script>
