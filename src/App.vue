34222f93b7f407a7674eec2b88f46dd60f3dcb65
Dasajton <david.sajitz.94@gmail.com>
2024-02-25 04:56:25 +0100

    Refactured App to use FavoriteChharacters as own component with props

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,26 @@
 <template>
   <h1 class="my-4 text-center text-4xl font-bold text-white">Avatar - The Last Airbender</h1>
-  <!-- Characters -->
+
   <CharacterList
     :characters="characterList"
     @remove="removeCharacter"
     @addFavorite="addFavoriteCharacter"
   />
-  <!-- Favorite Characters -->
-  <div class="mx-auto my-2 w-11/12 rounded-lg bg-slate-950 p-4 text-slate-100 md:w-3/5 xl:w-1/2">
-    <h2 class="p-2 text-center text-3xl">Favorite Characters</h2>
-    <ul class="mx-auto w-10/12 rounded bg-slate-900 p-2 text-slate-100">
-      <p v-if="favoriteList.length < 1" class="text-center text-xl">No favorites added, yet.</p>
-      <li
-        v-else
-        v-for="(character, index) in favoriteList"
-        :key="index"
-        class="my-2 flex items-center justify-between rounded border bg-slate-950 p-2 hover:bg-slate-800"
-      >
-        <p class="text-xl"><span class="text-green-500">></span> {{ character.name }}</p>
-        <button @click="removeFavoriteCharacter(character)" class="rounded bg-blue-500 px-2 py-1">
-          ❌
-        </button>
-      </li>
-    </ul>
-  </div>
-
-  <!-- Element Statistics -->
+  <FavoriteCharacters :favorites="favoriteList" @remove="removeFavoriteCharacter" />
   <BenderStatistics :characters="characterList" />
-  <!-- New Character -->
   <NewCharacter :characters="characterList" />
 </template>
 
 <script>
 import CharacterList from "./components/CharacterList.vue"
+import FavoriteCharacters from "./components/FavoriteCharacters.vue"
 import BenderStatistics from "./components/BenderStatistics.vue"
 import NewCharacter from "./components/NewCharacter.vue"
 
 export default {
   components: {
     CharacterList,
+    FavoriteCharacters,
     BenderStatistics,
     NewCharacter
   },
---
+++ src/components/FavoriteCharacters.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mx-auto my-2 w-11/12 rounded-lg bg-slate-950 p-4 text-slate-100 md:w-3/5 xl:w-1/2">
+    <h2 class="p-2 text-center text-3xl">Favorite Characters</h2>
+    <ul class="mx-auto w-10/12 rounded bg-slate-900 p-2 text-slate-100">
+      <p v-if="favorites.length < 1" class="text-center text-xl">No favorites added, yet.</p>
+      <li
+        v-else
+        v-for="(character, index) in favorites"
+        :key="index"
+        class="my-2 flex items-center justify-between rounded border bg-slate-950 p-2 hover:bg-slate-800"
+      >
+        <p class="text-xl"><span class="text-green-500">></span> {{ character.name }}</p>
+        <button @click="$emit('remove', character)" class="rounded bg-blue-500 px-2 py-1">
+          ❌
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    favorites: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
