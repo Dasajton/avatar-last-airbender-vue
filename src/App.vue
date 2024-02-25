94f260c8552b60761e847b6a9d1f34cde369ddbb
Dasajton <david.sajitz.94@gmail.com>
2024-02-25 04:50:04 +0100

    Refactor App.vue to use CharacterList component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template>
   <h1 class="my-4 text-center text-4xl font-bold text-white">Avatar - The Last Airbender</h1>
   <!-- Characters -->
-  <div class="mx-auto my-2 w-11/12 rounded-lg bg-slate-950 p-4 text-slate-100 md:w-3/5 xl:w-1/2">
-    <h2 class="p-2 text-center text-3xl">Characters</h2>
-    <ul class="mx-auto w-10/12 rounded bg-slate-900 p-2 text-slate-100">
-      <p v-if="characterList.length < 1" class="text-center text-xl">No Characters added, yet.</p>
-      <li
-        v-else
-        v-for="character in characterList"
-        class="my-2 flex items-center justify-between rounded border bg-slate-950 p-2 hover:bg-slate-800"
-      >
-        <p class="text-xl"><span class="text-green-500">></span> {{ character.name }}</p>
-        <div class="flex gap-2">
-          <button @click="addFavoriteCharacter(character)" class="rounded bg-blue-500 px-2 py-1">
-            ⭐
-          </button>
-          <button @click="removeCharacter(character)" class="rounded bg-blue-500 px-2 py-1">
-            ❌
-          </button>
-        </div>
-      </li>
-    </ul>
-  </div>
-
+  <CharacterList
+    :characters="characterList"
+    @remove="removeCharacter"
+    @addFavorite="addFavoriteCharacter"
+  />
   <!-- Favorite Characters -->
   <div class="mx-auto my-2 w-11/12 rounded-lg bg-slate-950 p-4 text-slate-100 md:w-3/5 xl:w-1/2">
     <h2 class="p-2 text-center text-3xl">Favorite Characters</h2>
@@ -49,11 +32,13 @@
 </template>
 
 <script>
+import CharacterList from "./components/CharacterList.vue"
 import BenderStatistics from "./components/BenderStatistics.vue"
 import NewCharacter from "./components/NewCharacter.vue"
 
 export default {
   components: {
+    CharacterList,
     BenderStatistics,
     NewCharacter
   },
---
+++ src/components/CharacterList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mx-auto my-2 w-11/12 rounded-lg bg-slate-950 p-4 text-slate-100 md:w-3/5 xl:w-1/2">
+    <h2 class="p-2 text-center text-3xl">Characters</h2>
+    <ul class="mx-auto w-10/12 rounded bg-slate-900 p-2 text-slate-100">
+      <p v-if="characters.length < 1" class="text-center text-xl">No Characters added, yet.</p>
+      <li
+        v-else
+        v-for="character in characters"
+        class="my-2 flex items-center justify-between rounded border bg-slate-950 p-2 hover:bg-slate-800"
+      >
+        <p class="text-xl"><span class="text-green-500">></span> {{ character.name }}</p>
+        <div class="flex gap-2">
+          <button @click="$emit('addFavorite', character)" class="rounded bg-blue-500 px-2 py-1">
+            ⭐
+          </button>
+          <button @click="$emit('remove', character)" class="rounded bg-blue-500 px-2 py-1">
+            ❌
+          </button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    characters: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
